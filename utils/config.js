1f291ac628ff97c750afae9faca0742c6101308c
Midyass <162825294+Midyass@users.noreply.github.com>
2025-06-07 17:32:10 +0100

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     prefix: '*',
-    token: 'MTM1NzcxOTA3NDgzMDAyNDc2NQ.GNhjfP.kw7D_KpKLN2eksC6ce9eLGZ9ebb71hpdnHaQQs',
+    token: '',
     clientId: '1357719074830024765',
     mongoURI: 'mongodb+srv://adaaaaammm:lCyDAMxXcC3Jrnu0@cluster0.se1qr.mongodb.net/',
   };
